9f42fcfbf662156e320947940b29f06a05febcf4
minjaesong <alswo9628@gmail.com>
2024-07-19 00:12:12 +0900

    windows and linux

--- src/Main.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by minjaesong on 2024-07-18.
- */
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
--- src/net/torvald/getbatterystatus/GetBatteryStatus.java
+++ src/net/torvald/getbatterystatus/GetBatteryStatus.java
@@ -28,6 +28,8 @@ public class GetBatteryStatus {
         }
     }
 
+    private static BatteryStatus nullStatus = new BatteryStatus(false, false, 0);
+
     /**
      * Don't call this function every frame or something, only poll the value sparingly (around 2 seconds)
      * @return
@@ -42,11 +44,14 @@ public class GetBatteryStatus {
             case OSX:
                 return getBattMac();
         };
-        return null;
+        return nullStatus;
     }
 
     private static BatteryStatus getBattWin() throws IOException {
-        return null;
+        Kernel32.SYSTEM_POWER_STATUS batteryStatus = new Kernel32.SYSTEM_POWER_STATUS();
+        Kernel32.INSTANCE.GetSystemPowerStatus(batteryStatus);
+
+        return batteryStatus.toBatteryStatus();
     }
 
     private static BatteryStatus getBattMac() throws IOException {
@@ -60,15 +65,66 @@ public class GetBatteryStatus {
         Matcher mat = pat.matcher(l2);
 
         boolean charging = l1.contains("'AC Power'");
-        mat.find();
-        String percentageStr = mat.group();
-        int perc = Integer.parseInt(percentageStr);
+        if (mat.find()) {
+            String percentageStr = mat.group();
+            int perc = Integer.parseInt(percentageStr);
 
-        return new BatteryStatus(true, charging, perc);
+            return new BatteryStatus(true, charging, perc);
+        }
+        else return nullStatus;
     }
 
     private static BatteryStatus getBattLinux() throws IOException {
-        return null;
+        // check if battery is there
+        // if the system has one, the output of `upower -e` should contain
+        // `/org/freedesktop/UPower/devices/battery_BAT0`
+        BufferedReader br1 = runCmdAndGetReader("upower -e");
+        boolean hasBat = false;
+        String lineRead;
+        while ((lineRead = br1.readLine()) != null) {
+            if (lineRead.startsWith("/org/freedesktop/UPower/devices/battery_BAT0")) {
+                hasBat = true;
+                break;
+            }
+        }
+
+        if (!hasBat) return nullStatus;
+
+        BufferedReader br2 = runCmdAndGetReader("upower -i /org/freedesktop/UPower/devices/battery_BAT0");
+
+        boolean lineFound = false;
+        boolean charging = false;
+        while ((lineRead = br2.readLine()) != null) {
+            // search for the line "    percentage:     00%" or "    percentage:     00.0000%"
+            if (lineRead.matches("(?<=capacity:)[\\t ]+[0-9]+(\\.[0-9]+)?(?=%)")) {
+                lineFound = true;
+            }
+            // search for the line "    state:   charging"
+            else if (lineRead.matches("(?<=[\\t ])charging")) {
+                charging = true;
+            }
+        }
+
+        if (!lineFound) return nullStatus;
+
+        Pattern pat = Pattern.compile("(?<=[\\t ])[0-9]+(\\.[0-9]+)?(?=%)");
+        Matcher mat = pat.matcher(lineRead);
+        if (mat.find()) {
+            // get substring before '.'
+            String percentageStr0 = mat.group();
+            int dotIndex = percentageStr0.indexOf('.');
+            int percentage;
+            if (dotIndex < 0) {
+                percentage = Integer.parseInt(percentageStr0);
+            }
+            else {
+                String percentageStr1 = percentageStr0.substring(0, dotIndex);
+                percentage = Integer.parseInt(percentageStr1);
+            }
+
+            return new BatteryStatus(hasBat, charging, percentage);
+        }
+        else return nullStatus;
     }
 
 
